ad8d13d784297d79c510a229c89857f5fc90e2f9
angelo <angelo@fluxton.co.uk>
2022-09-25 14:21:27 +0100

    started pages

--- js/scripts.js
+++ js/scripts.js
@@ -22,26 +22,53 @@ close.addEventListener('click', () => {
 
 // AQ carousel
 
-let w = window.innerWidth
-let h = window.innerHeight
+// let w = window.innerWidth
+// let h = window.innerHeight
 
-const imgs = document.getElementById('imgs')
-const img = document.querySelectorAll('#imgs img')
+// const imgs = document.getElementById('imgs')
+// const img = document.querySelectorAll('#imgs img')
 
-let idx = 0
-let interval = setInterval(run, 2000)
+// let idx = 0
+// let interval = setInterval(run, 2000)
 
-function run() {
-  idx++
-  changeImage()
+// function run() {
+//   idx++
+//   changeImage()
+// }
+
+// function changeImage() {
+//   if (idx > img.length - 1) {
+//     idx = 0
+//   } else if (idx < 0) {
+//     idx = img.length -1
+//   }
+
+//   imgs.style.transform = `translateX(${-idx * 800}px)`
+// }
+
+//AQ modal
+var modal = document.getElementById('myModal')
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+
+// put images into an array
+// add click handler for each
+// on click, get THAT image and alt
+// ...and do the do!
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
 }
 
-function changeImage() {
-  if (idx > img.length - 1) {
-    idx = 0
-  } else if (idx < 0) {
-    idx = img.length -1
-  }
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  imgs.style.transform = `translateX(${-idx * 800}px)`
-}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
